d82ee7a23611043f5d9ed6a3425dc0e643ad98c2
Cesar <mongee14@gmail.com>
2024-07-08 16:34:01 -0600

    Primer Avance C1

---
+++ parte_superior.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="English">
+
+<head>
+    <meta chartset="utf-8">
+    <meta name="Proyecto de Tercera 1" content="Página de F1">
+    <title>
+        F1 Inicio
+    </title>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+
+<menu>
+    <div class="menu-izquierda">
+        <a href="./index.php">
+            <img src="./images/menu.png" alt="">
+        </a>
+        <p class="texto-menu-izquierda">
+            Formula 1
+        </p>
+    </div>
+</menu>
